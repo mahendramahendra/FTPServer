b6ddc3b5925db7017a54ce1690880b270a97c584
mahendramahendra <31504600+mahendramahendra@users.noreply.github.com>
2025-06-05 13:47:07 +0100

    Add files via upload

---
+++ src/main/java/com/ftp/process/FtpProcessApplication.java
@@ -0,0 +1,13 @@
+package com.ftp.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftp/process/FtpProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftp.process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtpProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
